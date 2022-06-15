4bfd869477818f136ab41800afac79304e686400
DRANIX <48390633+dran1x@users.noreply.github.com>
2022-06-15 13:15:46 -0400

    Update

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-SERVERDIR=/mnt/server
-STEAMCMDDIR=mnt/server/steamcmd
-
 cd /tmp
 mkdir -p ${SERVERDIR}/steamcmd
-wget -qO- 'https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz'
-tar -xzvf steamcmd.tar.gz -C ${STEAMCMDDIR}
+wget -qO- "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar xvzf - -C "${STEAMCMDDIR}"
 ./${STEAMCMDDIR}/steamcmd.sh +quit
 
 mkdir -p "${SERVERDIR}/.steam/sdk32"
@@ -13,3 +9,15 @@ mkdir -p "${SERVERDIR}/.steam/sdk32"
 ln -s "${STEAMCMDDIR}/linux32/steamclient.so" "${SERVERDIR}/.steam/sdk32/steamclient.so"
 ln -s "${STEAMCMDDIR}/linux32/steamcmd" "${STEAMCMDDIR}/linux32/steam"
 ln -s "${STEAMCMDDIR}/steamcmd.sh" "${STEAMCMDDIR}/steam.sh"
+
+mkdir -p ${SERVERDIR}/steamapps
+
+cd ${SERVERDIR}/steamcmd
+
+chown -R root:root /mnt
+export HOME=/mnt/server
+
+bash "${STEAMCMDDIR}/steamcmd.sh" +force_install_dir "${STEAMAPPDIR}" \
+				+login anonymous \
+				+app_update "${STEAMAPPID}" \
+				+quit
