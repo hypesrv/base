8a08bc1ca0ef6b0aaf77a8b36504beb085acfeb9
DRANIX <48390633+dran1x@users.noreply.github.com>
2022-06-15 15:34:13 -0400

    Update

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
-STEAM_USER=anonymous
-STEAM_PASS=""
-STEAM_AUTH=""
 
 cd /tmp
-mkdir -p /mnt/server/steamcmd
-wget -qO- "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar xvzf - -C /mnt/server/steamcmd
-mkdir -p /mnt/server/steamapps
-cd /mnt/server/steamcmd
+mkdir -p ${SRVMNT}/steamcmd ${SRVMNT}/steamapps
+wget -qO- "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar xvzf - -C "${SRVMNT}/steamcmd"
+cd ${SRVMNT}/steamcmd
 
 chown -R root:root /mnt
-export HOME=/mnt/server
 
-./steamcmd.sh +force_install_dir /mnt/server +login ${STEAM_USER} ${STEAM_PASS} ${STEAM_AUTH} +app_update ${SRCDS_APPID} ${EXTRA_FLAGS} +quit ## other flags may be needed depending on install. looking at you cs 1.6
+./steamcmd.sh +force_install_dir ${SRVMNT} +login ${STEAM_USER} ${STEAM_PASS} ${STEAM_AUTH} +app_update ${STEAMAPPID} ${EXTRA_FLAGS} +quit
 
-mkdir -p /mnt/server/.steam/sdk32
+mkdir -p ${SRVMNT}/.steam/sdk32
 cp -v linux32/steamclient.so ../.steam/sdk32/steamclient.so
 
-# mkdir -p /mnt/server/.steam/sdk64
-# cp -v linux64/steamclient.so ../.steam/sdk64/steamclient.so
+if  [ ! -z "$METAMOD_VERSION" ] && [ ! -d "${SRVMNT}/${STEAMAPP}/addons/metamod" ]; then
+	LATESTMM=$(wget -qO- https://mms.alliedmods.net/mmsdrop/"${METAMOD_VERSION}"/mmsource-latest-linux)
+	wget -qO- https://mms.alliedmods.net/mmsdrop/"${METAMOD_VERSION}"/"${LATESTMM}" | tar xvzf - -C "${SRVMNT}"	
+fi
+
+if  [ ! -z "$SOURCEMOD_VERSION" ] && [ ! -d "${SRVMNT}/${STEAMAPP}/addons/sourcemod" ]; then
+	LATESTSM=$(wget -qO- https://sm.alliedmods.net/smdrop/"${SOURCEMOD_VERSION}"/sourcemod-latest-linux)
+	wget -qO- https://sm.alliedmods.net/smdrop/"${SOURCEMOD_VERSION}"/"${LATESTSM}" | tar xvzf - -C "${SRVMNT}"
+fi
